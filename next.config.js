a17c79bfad646537d95b646e2450021e123f87dc
David Inga <david.inga@vizzuality.com>
2022-11-15 09:54:28 +0100

    testing geoparquet and deckgl

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: function (config, { isServer }) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+    return config
+  }
 }
 
 module.exports = nextConfig
