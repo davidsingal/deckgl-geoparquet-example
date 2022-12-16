1ebd3395f1bc596cb08ca97bfcb92fa697f07eec
David Inga <david.inga@vizzuality.com>
2022-12-16 11:10:07 +0100

    new data and some improvements

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['src'],
+  },
   reactStrictMode: true,
   swcMinify: true,
   webpack: function (config, { isServer }) {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true }
-    return config
-  }
-}
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
